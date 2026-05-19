de4dec9f8f293f530a0db94f65e00931b6111688
itsAishbaa <aalishbaa3@gmail.com>
2026-05-19 11:15:23 -0700

    adding db

--- Components/Data/AppDbContext.cs
+++ Components/Data/AppDbContext.cs
@@ -1,6 +1,21 @@
-﻿namespace final_pro_c.Components.Data
-{
-    public class AppDbContext
-    {
-    }
-}
+﻿//using final_pro_c.Models;
+//using IronCrusadeBlazor.Models;
+//using Microsoft.EntityFrameworkCore;
+//using System.Reflection.Emit;
+
+//namespace IronCrusadeBlazor.Data
+//{
+//    public class AppDbContext : DbContext
+//    {
+//        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+//        public DbSet<User> Users => Set<User>();
+//        public DbSet<MatchHistory> Matches => Set<MatchHistory>();
+
+//        protected override void OnModelCreating(ModelBuilder modelBuilder)
+//        {
+//            modelBuilder.Entity<User>().HasIndex(u => u.Username).IsUnique();
+//            modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+//        }
+//    }
+//}
